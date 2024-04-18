f40f66c795834b35da843b93678d835947a2be5d
Stephen Ibeh <steve.junior@gmail.com>
2024-04-18 09:17:31 -0400

    Game activity/runner set up

---
+++ README.md
@@ -0,0 +1,4 @@
+### Navigate the project directory and run 
+`pip3 install -r requirements.txt`
+
+### To run the project, use `python runner.py`
---
+++ tictactoe.py
@@ -0,0 +1,18 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
